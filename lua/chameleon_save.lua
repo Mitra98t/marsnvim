dbc9d77b535e21ca24baed77deb402998fcc2dfa
Mitra98t <personalmailfm98@gmail.com>
2025-02-01 15:53:31 +0100

    .

--- lua/chameleon_save.lua
+++ lua/chameleon_save.lua
@@ -1,6 +1,6 @@
 -- chameleon.nvim
 -- This file is used to reload your theme settings on restart
-local colorscheme = 'catppuccin-mocha'
+local colorscheme = 'gruvbox'
 local before = nil
 local after = nil
 return { colorscheme = colorscheme, before = before, after = after }
